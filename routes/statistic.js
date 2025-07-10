470aaa9bf17662d32f8e9cd4a557cf0a27274d07
arisaputra1717 <saputrari705@gmail.com>
2025-07-11 00:40:31 +0700

    Delete routes directory

--- routes/statistic.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statisticController = require('../controllers/statisticController');
-
-router.get('/', statisticController.index);
-router.get('/data', statisticController.getChartData); // endpoint AJAX untuk grafik
-
-module.exports = router;
